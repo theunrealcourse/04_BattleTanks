7125a7aef61ca905699f8778a34a9c195536d1da
smccar25 <smccarthy942@googlemail.com>
2019-03-03 12:30:40 +0000

    BT39 - Barrel Pitches with aiming component

--- BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
@@ -5,6 +5,11 @@
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
 
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
