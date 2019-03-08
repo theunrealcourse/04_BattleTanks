78a63955845e4abc88cda7d8bc3c6b030a28397f
smccar25 <smccarthy942@googlemail.com>
2019-03-08 20:44:07 +0000

    BT47 - Added Thrttole Inputs and TankTrack Class

--- BattleTanks/Source/BattleTanks/Private/Projectile.cpp
+++ BattleTanks/Source/BattleTanks/Private/Projectile.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
 
 
 // Sets default values
