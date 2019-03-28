ede74530586702c847733e16ca3dd28d58b22aee
smccar25 <smccarthy942@googlemail.com>
2019-03-28 13:29:02 +0000

    BT60 - Used enum and set up firing state

--- BattleTanks/Source/BattleTanks/Public/TankAimingComponent.h
+++ BattleTanks/Source/BattleTanks/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for Aiming State
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 // Forward Declaration
 class UTankBarrel; 
 class UTankTurret;
@@ -21,21 +30,19 @@ public:
 	UTankAimingComponent();
 
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
-
 	void SetTurretReference(UTankTurret* TurretToSet);
-
-protected:
-
-public:	
-
-	// TODO add SetTurretReference
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 	
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
-
 	UTankTurret* Turret = nullptr;
+
+
 
 	void MoveBarrelTowards(FVector AimDirection);
 
